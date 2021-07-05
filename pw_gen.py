e80d2ec6fcf675c74ed24b659038252f322cbc6c
Nick McDaniel <nickamcd@gmail.com>
2021-07-05 15:46:50 -0700

    Add group for verbose/quiet

--- pw_gen.py
+++ pw_gen.py
@@ -16,6 +16,10 @@ def main():
   parser.add_argument('-ns', '--no-symbols', action='store_true', \
     help='Flag to disable symbols in pw generation')
 
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('-v', '--verbose', action='store_true')
+  group.add_argument('-q', '--quiet', action='store_true')
+
 
   args = parser.parse_args()
 
