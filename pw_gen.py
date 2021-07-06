306161b4055b4fe7f8853500d605f97b023a23f2
Nick McDaniel <nickamcd@gmail.com>
2021-07-05 18:33:22 -0700

    Added basic documentation

--- pw_gen.py
+++ pw_gen.py
@@ -1,30 +1,66 @@
 import argparse
 
-def main():
-  # Set up argument parser
-  parser = argparse.ArgumentParser(description='Simple Password Generator')
+__version__ = '0.1.0'
 
-  parser.add_argument('-l', '--length', metavar='<int>', type=int, \
-    default=12, help='Set the length of the password (default length: 12)')
+def length(length):
+  """
+  Alters the length of the password string that is generated.
 
-  parser.add_argument('-s', '--save', action='store_true', \
-    help='Save the password to passwords.txt file')
+  Parameters
+  ----------
+    length : int
+      Length of the password generated
+  """
+  print(str(length))
 
-  parser.add_argument('-nn', '--no-numbers', action='store_true', \
-    help='Flag to disable nums in pw generation')
+def save():
+  """
+  Saves the password to the local passwords.txt file.
+  """
+  print('save')
 
-  parser.add_argument('-ns', '--no-symbols', action='store_true', \
-    help='Flag to disable symbols in pw generation')
+def main(args):
+  """
+  Driver function.
 
-  group = parser.add_mutually_exclusive_group()
-  group.add_argument('-v', '--verbose', action='store_true')
-  group.add_argument('-q', '--quiet', action='store_true')
-
-
-  args = parser.parse_args()
-
+  Parameters
+  ----------
+    args : list
+      command line arguments
+  """
   print(args)
+
+  if(args.length):
+    length(args.length)
+
+  if (args.save):
+    save()
+
+def parse_arguments():
+  """
+  Creates arg parser and returns parsed args.
+  """
+  version = '%(prog)s ' + __version__
+
+  parser = argparse.ArgumentParser(description='Simple Password Generator')
+  parser.add_argument('-l', '--length', metavar='<int>', type=int, \
+                      default=12, help='Set the length of the password (default length -> 12)')
+  parser.add_argument('-s', '--save', action='store_true', \
+                      help='Save the password to passwords.txt file')
+  parser.add_argument('-nn', '--no-numbers', action='store_true', \
+                      help='Flag to disable nums in pw generation')
+  parser.add_argument('-ns', '--no-symbols', action='store_true', \
+                      help='Flag to disable symbols in pw generation')
+  parser.add_argument('-V', '--version', action='version', version=version, \
+                      help='Display version')
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('-v', '--verbose', action='store_true', \
+                      help='Increase output verbosity')
+  group.add_argument('-q', '--quiet', action='store_true', \
+                      help='Decrease output verbosity')
+  return parser.parse_args()
 
 
 if __name__ == '__main__':
-  main()
+  args = parse_arguments()
+  main(args)
