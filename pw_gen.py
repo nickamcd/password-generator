3473ae31662ba6aae8a25d71b90fa6579dee2a59
Nick McDaniel <nickamcd@gmail.com>
2021-07-06 11:24:25 -0700

    Refactored code

--- pw_gen.py
+++ pw_gen.py
@@ -1,6 +1,7 @@
 import argparse
+import pyperclip
 from random import randrange
-from colorama import Fore, Back, Style, init
+from colorama import Fore, Style, init
 
 __version__ = '0.1.0'
 
@@ -16,24 +17,23 @@ def save():
   """
   print('save')
 
-def generate_password(length, no_lower=False, no_upper=False, no_numbers=False, no_symbols=False):
+def get_password(length, save, no_lower=False, no_upper=False,
+                  no_numbers=False, no_symbols=False):
   """
-  Generate the password by selecting random chars.
+  Get password and generate output.
 
   Parameters
   ----------
     length : int
       Length of the password
-
+    save : bool
+      Flag for if the pw will be saved to text file
     no_lower : bool
       Flag for if pw has lowercase letters or not
-
     no_upper : bool
       Flag for if pw has uppercase letters or not
-    
     no_numbers : bool
       Flag for if pw has numbers or not
-
     no_symbols : bool
       Flag for if pw has symbols or not
   """
@@ -48,12 +48,30 @@ def generate_password(length, no_lower=False, no_upper=False, no_numbers=False, 
   if len(chars) == 0:
     return 'Unable to generate password'
 
-  password = ''
+  password = generate_password(length, chars)
 
-  for i in range(length):
-    password = password + chars[randrange(len(chars))]
+  # copy to clipboard
+  pyperclip.copy(password)
+  print(Fore.MAGENTA + Style.BRIGHT + 'Password copied to clipboard.')
 
   return Fore.CYAN + 'Password: ' + Fore.GREEN + Style.BRIGHT + password
+
+def generate_password(length, chars):
+  """
+  Generate password.
+
+  Parameters
+  ----------
+    length : int
+      Length of the password
+    chars : str
+      String of allowed chars
+  """
+  password = ''
+  for i in range(length):
+    password = password + chars[randrange(len(chars))]
+  return password
+
 
 def main(args):
   """
@@ -66,14 +84,14 @@ def main(args):
   """
   init(autoreset=True) # colorama init
   print(args)
+  output = get_password(args.length,
+                        args.save, 
+                        args.no_lower, 
+                        args.no_upper,
+                        args.no_numbers, 
+                        args.no_symbols)
 
-  password = generate_password( args.length, 
-                                args.no_lower, 
-                                args.no_upper,
-                                args.no_numbers, 
-                                args.no_symbols )
-
-  print(password)
+  print(output)
 
 def parse_arguments():
   """
@@ -83,7 +101,7 @@ def parse_arguments():
 
   parser = argparse.ArgumentParser(description='Simple Password Generator')
   parser.add_argument('-l', '--length', metavar='<int>', type=int, \
-                      default=12, help='Set the length of the password (default length -> 12)')
+                      default=12, help='Set the len of password (default -> 12)')
   parser.add_argument('-s', '--save', action='store_true', \
                       help='Save the password to passwords.txt file')
   parser.add_argument('-nl', '--no-lower', action='store_true', \
